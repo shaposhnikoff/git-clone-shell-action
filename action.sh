d5e080315df3c9d2d69e94137ddef87c309ebe32
Shaposhnikoff <shaposhnikoff@gmail.com>
2023-04-15 14:10:15 +0000

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-set -eux
-git clone --progress --single-branch --branch "${INPUTS_BRANCH:-${GITHUB_REF##*/}}" "https://${GITHUB_REPOSITORY_OWNER}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" .
+#set -eux
+#git clone --progress --single-branch --branch "${INPUTS_BRANCH:-${GITHUB_REF##*/}}" "https://${GITHUB_REPOSITORY_OWNER}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" .
+
+if jq -e . >/dev/null 2>&1 <<<"$json_string"; then
+    echo "Parsed JSON successfully and got something other than false/null"
+else
+    echo "Failed to parse JSON, or got false/null"
+fi
